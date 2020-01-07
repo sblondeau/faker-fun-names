c99f4eb7ef5b4826e669785c704d495902b5dcd9
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-01-07 22:57:35 +0100

    fun names provider for faker

---
+++ tests/FunDataProviderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Tests;
+
+use Faker\Factory;
+use FunDataProvider\FunDataProvider;
+use PHPUnit\Framework\TestCase;
+
+class FunDataProviderTest extends TestCase
+{
+    public function testFunData()
+    {
+        $faker = Factory::create();
+        $faker->addProvider(new FunDataProvider($faker));
+        $this->assertContains($faker->funName, FunDataProvider::$names);
+    }
+
+}
